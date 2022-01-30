e6865878f97b5b128a211935cd9c2893827a7eeb
Sumair <95204672+Sumair134574@users.noreply.github.com>
2022-01-30 12:55:08 +0000

    a

---
+++ p2final.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+input()
+{
+  int a;
+  printf("enter a number\n");
+  scanf(%d,&a);
+  return a;
+}
+int find_sum(int a,int b)
+{
+  int sum;
+  sum=a+b;
+  return sum;
+}
+void output(int a,int b, int c);
+{
+  printf("sumof %d+%d is %d\",a,b,c");
+}
+int main()
+{
+  int x,y,z;
+  x=input*();
+  y=input();
+  z=find_sum(x,y);
+  outout(x,y,z);
+  return 0;
+  }
